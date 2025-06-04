11c50fa8c4a5da57c01f7c3e4a99a422d25c92e0
cheng827922 <cheng827922@outlook.com>
2025-06-04 16:27:16 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,19 @@
+function getShowLed () {
+	
+}
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let x;
+basic.forever(function () {
+	
+})
